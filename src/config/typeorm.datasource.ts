ef8dff25b6679ecc76ffb6c9c936e0fcd8de77d8
LumSeg <lms.segovia86@gmail.com>
2025-11-25 10:22:40 -0300

    " "

--- src/config/typeorm.datasource.ts
+++ src/config/typeorm.datasource.ts
@@ -18,6 +18,7 @@ export default new DataSource({
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
   synchronize: false,
+  dropSchema: true,
   logging: true,
   ssl: process.env.DB_SSL === 'true' ? {
     rejectUnauthorized: false,
